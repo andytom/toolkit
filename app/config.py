14eadebbd7fcf7bc13b8a6937c70f44523107f5f
Thomas O'Donnell <andy.tom@gmail.com>
2015-02-08 23:48:46 +0000

    Clean up of the config file

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
 import os
+
+# See the Flask docs for how to generate a good secret key
+# http://flask.pocoo.org/docs/0.10/quickstart/#sessions
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = False
