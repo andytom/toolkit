65b1b4419669b2446d32d0ab24f33eea702f7d0c
Thomas O'Donnell <andy.tom@gmail.com>
2015-02-08 20:47:34 +0000

    Fixing UTF-8 decoding/encoding for table_maker

--- app/table_maker/__init__.py
+++ app/table_maker/__init__.py
@@ -1,4 +1,5 @@
-from io import StringIO
+import csv
+from StringIO import StringIO
 from flask import Blueprint, render_template, g
 from flask_wtf import Form
 import prettytable
@@ -20,12 +21,35 @@ def before_request():
 #-----------------------------------------------------------------------------#
 # Helper methods
 #-----------------------------------------------------------------------------#
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
+
+
 def process_string(csv_string, table_align='l'):
     """Take a raw csv string and convert it into a table"""
-    # TODO - Does this work with non ASCII characters?
     csv_io = StringIO(csv_string)
 
-    table = prettytable.from_csv(csv_io)
+    dialect = csv.Sniffer().sniff(csv_io.read(1024))
+    csv_io.seek(0)
+
+    reader = unicode_csv_reader(csv_io, dialect)
+
+    table = prettytable.PrettyTable()
+
+    table.field_names = [x.strip() for x in reader.next()]
+
+    for row in reader:
+        table.add_row([x.strip() for x in row])
 
     table.align = table_align
 
