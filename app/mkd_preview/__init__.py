06b8850a1dffdaa3804963d86f982b69bf37f061
Thomas O'Donnell <andy.tom@gmail.com>
2015-02-21 19:18:12 +0000

    Moved example text out of the template.

--- app/mkd_preview/__init__.py
+++ app/mkd_preview/__init__.py
@@ -4,6 +4,24 @@ from flask import Blueprint, render_template, g
 mkd_preview = Blueprint('mkd_preview', __name__, template_folder='templates')
 
 
+EXAMPLE_MARKDOWN_TEXT = """# Title
+
+Some example text
+
+## Sub Title
+
+Some code
+
+~~~
+for i in range(10):
+    print i
+~~~
+
+## Another Sub Title
+
+A link to the [Github Flavoured Markdown documentaion](https://help.github.com/articles/github-flavored-markdown/)."""
+
+
 @mkd_preview.before_request
 def before_request():
     g.title = 'Markdown Preview'
@@ -11,4 +29,5 @@ def before_request():
 
 @mkd_preview.route('/')
 def index():
-    return render_template('mkd_preview/index.html')
+    return render_template('mkd_preview/index.html',
+                           content=EXAMPLE_MARKDOWN_TEXT)
