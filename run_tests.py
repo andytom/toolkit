e64f2bc08a3f34643b9687f5d577393bd9b8914f
Thomas O'Donnell <andy.tom@gmail.com>
2015-03-25 22:45:19 +0000

    Updating test runner script.

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,9 @@
 import unittest
 
+def run_all_tests():
+    tests = unittest.TestLoader().discover('./app')
+    results = unittest.TextTestRunner().run(tests)
 
-tests = unittest.TestLoader().discover('./app')
-results = unittest.TextTestRunner().run(tests)
+
+if __name__ == '__main__':
+    run_all_tests()
