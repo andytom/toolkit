3546cddc480d7528dc9576280eed4cc0f105bf43
Thomas O'Donnell <andy.tom@gmail.com>
2015-04-06 21:55:58 +0100

    More table_maker tests.

--- app/table_maker/tests.py
+++ app/table_maker/tests.py
@@ -10,9 +10,23 @@ class BaseTestCase(TestCase):
     def create_app(self):
         app = Flask(__name__)
         app.config['TESTING'] = True
-        app.config['SECRET_KEY'] = 'TEST'
+        app.config['WTF_CSRF_ENABLED'] = False
         app.register_blueprint(table_maker.table_maker)
         return app
+
+    def _load_data_from_file(self, filename):
+        cwd = os.path.dirname(__file__)
+        resources = os.path.join(cwd, 'resources')
+        input_file = os.path.join(resources, '{}.csv'.format(filename))
+        output_file = os.path.join(resources, '{}.out'.format(filename))
+
+        with codecs.open(input_file, 'r', 'utf8') as f:
+            csv = f.read()
+
+        with codecs.open(output_file, 'r', 'utf8') as f:
+            expected = f.read()
+
+        return csv, expected
 
 
 #-----------------------------------------------------------------------------#
@@ -28,18 +42,8 @@ class ProcessStringTestCase(BaseTestCase):
         There must exist a filename.csv and a filename.out in the resources
         dir.
         """
-        cwd = os.path.dirname(__file__)
-        resources = os.path.join(cwd, 'resources')
-        input_file = os.path.join(resources, '{}.csv'.format(filename))
-        output_file = os.path.join(resources, '{}.out'.format(filename))
-
-        with codecs.open(input_file, 'r', 'utf8') as f:
-            csv = f.read()
-            res = table_maker.process_string(csv, **options)
-
-        with codecs.open(output_file, 'r', 'utf8') as f:
-            expected = f.read()
-
+        csv, expected = self._load_data_from_file(filename)
+        res = table_maker.process_string(csv, **options)
         self.assertEqual(res, expected)
 
     def test_simple_file(self):
@@ -68,3 +72,23 @@ class AppTestCase(BaseTestCase):
     def test_page_load(self):
         rv = self.client.get("/")
         self.assertEqual(rv.status_code, 200)
+        self.assertTrue("Table Maker" in rv.data)
+
+    def test_valid_form_submit(self):
+        csv, out = self._load_data_from_file('alignment_l')
+        rv = self.client.post('/',
+            data={'csv_string':csv, 'table_align':'l'})
+        self.assertEqual(rv.status_code, 200)
+        self.assertTrue(out in rv.data)
+
+    def test_valid_form_submit_alignment(self):
+        csv, out = self._load_data_from_file('alignment_c')
+        rv = self.client.post('/',
+            data={'csv_string':csv, 'table_align':'c'})
+        self.assertEqual(rv.status_code, 200)
+        self.assertTrue(out in rv.data)
+
+    def test_empty_form_submit(self):
+        rv = self.client.post('/')
+        self.assertFalse('Output' in rv.data)
+
