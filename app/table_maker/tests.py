170bb8563d90bac48ee8c28dda9ebe3d0adbcefd
Thomas O'Donnell <andy.tom@gmail.com>
2015-04-06 13:23:06 +0000

    Adding lots more tests to table_maker.

--- app/table_maker/tests.py
+++ app/table_maker/tests.py
@@ -5,7 +5,15 @@ import table_maker
 
 
 class ProcessStringTestCase(unittest.TestCase):
-    def _test_from_file(self,filename):
+    def _test_from_file(self, filename, **options):
+        """
+        Helper method to wrap the running of tests from a file.
+
+        Takes an input filename and optional args for process_string.
+
+        There must exist a filename.csv and a filename.out in the resources
+        dir.
+        """
         cwd = os.path.dirname(__file__)
         resources = os.path.join(cwd, 'resources')
         input_file = os.path.join(resources, '{}.csv'.format(filename))
@@ -13,7 +21,7 @@ class ProcessStringTestCase(unittest.TestCase):
 
         with codecs.open(input_file, 'r', 'utf8') as f:
             csv = f.read()
-            res = table_maker.process_string(csv)
+            res = table_maker.process_string(csv, **options)
 
         with codecs.open(output_file, 'r', 'utf8') as f:
             expected = f.read()
@@ -25,3 +33,15 @@ class ProcessStringTestCase(unittest.TestCase):
 
     def test_complex_file(self):
         self._test_from_file('complex')
+
+    def test_trailing_whitespace(self):
+        self._test_from_file('trailing_whitespace')
+
+    def test_alignment_left(self):
+        self._test_from_file('alignment_l', table_align='l')
+
+    def test_alignment_center(self):
+        self._test_from_file('alignment_c', table_align='c')
+
+    def test_alignment_right(self):
+        self._test_from_file('alignment_r', table_align='r')
