7abfa8c23a1ce7e03e07aec86f902900732490d2
Thomas O'Donnell <andy.tom@gmail.com>
2015-04-09 01:41:28 +0100

    Starting to add tests to base64_utils

--- app/base64_utils/tests.py
+++ app/base64_utils/tests.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask.ext.testing import TestCase
 
@@ -11,6 +12,24 @@ class BaseTestCase(TestCase):
         app.config['WTF_CSRF_ENABLED'] = False
         app.register_blueprint(base64_utils.base64_utils)
         return app
+
+    def _gen_full_path(self, filename):
+        cwd = os.path.dirname(__file__)
+        resources = os.path.join(cwd, 'resources')
+        return os.path.join(resources, filename)
+
+
+class ProcessBase64TestCase(BaseTestCase):
+    def test_creates_unique_filenames(self):
+        f = base64_utils.create_filename('.txt')
+        g = base64_utils.create_filename('.txt')
+        self.assertFalse(f == g)
+
+    def test_guess_extention_txt(self):
+        filename = self._gen_full_path('test.txt')
+        with open(filename, 'rb') as f:
+            res = base64_utils.guess_extention(f.read())
+        self.assertEqual(res, '.txt')
 
 
 class AppTestCase(BaseTestCase):
